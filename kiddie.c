bf839b4a565764e82973d07ad520ee3de67b617e
DefaulT <75148247+xlt-xau-xef-xde@users.noreply.github.com>
2020-12-10 21:15:32 +0100

    Update kiddie.c

--- kiddie.c
+++ kiddie.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
- 
-void proof() {
-    setreuid(geteuid(), geteuid());
-    system("/bin/bash");
-}
-
-void func(){
-
-    char buffer[64];
-    fgets(buffer,69,stdin);
-}
 
 int main(){
 
-    printf("Hello, good ! This is the second part of the challenge\n");
-    printf("\n\nFirst tell me your name :\n\n");
-    char buffer[128];
-    fgets(buffer,133,stdin);
-    printf("\nSecond tell me your age :\n\n");
-    func();
+    printf("sorry the source code is not leak\n");
+    printf("désolé le code source n'est pas publié\n");
+    printf("Programme écrit en C")
+    printf("written in C"
     return(0);
 }
