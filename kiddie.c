942e4ff099e619bef8177a4510ad3812bba6114c
DefaulT <75148247+xlt-xau-xef-xde@users.noreply.github.com>
2020-12-11 02:08:20 +0100

    Update kiddie.c

--- kiddie.c
+++ kiddie.c
@@ -2,8 +2,8 @@
 
 int main(){
 
-    printf("sorry the source code is not leak\n");
-    printf("désolé le code source n'est pas publié\n");
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
     printf("Programme écrit en C")
     printf("written in C"
     return(0);
