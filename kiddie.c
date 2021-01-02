ee69436dd5da4279c112e312f0a00e6acf7a3330
DefaulT <75148247+xlt-xau-xef-x0d@users.noreply.github.com>
2021-01-02 06:35:39 +0100

    Update kiddie.c

--- kiddie.c
+++ kiddie.c
@@ -8,3 +8,93 @@ int main(){
     printf("written in C"
     return(0);
 }
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
+#include <stdio.h>
+
+int main(){
+
+    printf("sorry the source code is not leak to no cheat\n");
+    printf("désolé le code source n'est pas publié pour ne pas triché\n");
+    printf("Programme écrit en C")
+    printf("written in C"
+    return(0);
+}
